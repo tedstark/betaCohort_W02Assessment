a527d9f390c533d64d81ebcd02e09d07df6e4429
Ted <ted.stark@gmail.com>
2018-01-19 15:13:04 -0700

    core app working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,21 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Write a guessing game program.
-        // Your program should generate a random integer between 1 and 10000.
-        // Prompt the user to enter a guess.
-        // For each guess, output "HIGHER" if the guess is smaller than the random integer
-        // or "LOWER" if the user's guess is larger than the random integer.
-        // Also output the new range of eligible guesses.
-        // When the user guesses the random integer, output a message that says so
+        // Write a guessing game program. [ ]
+        // Your program should generate a random integer between 1 and 10000. [ ]
+        // Prompt the user to enter a guess. [x]
+        // For each guess, output "HIGHER" if the guess is smaller than the random integer  [ ]
+        // or "LOWER" if the user's guess is larger than the random integer.  [ ]
+        // Also output the new range of eligible guesses.  [ ]
+        // When the user guesses the random integer, output a message that says so [X]
         // and displays the number guesses it took to get to the right number.
         // Additionally, it should show what numbers the user guessed along the way.
 
@@ -44,23 +47,61 @@ public class Main {
             // random number block
             Random random = new Random();
             int secretNumber = random.nextInt(10000);
+
             // code to pause while "thinking"
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException pause) {
                 pause.printStackTrace();
             }
-            System.out.println("Okay, got the number!");
-            System.out.println("My secret number is: " + secretNumber);
+            //get player input System.out.println("Okay, got the number!");
+            System.out.println("My secret number is: " + secretNumber); //print statement for testing/troubleshooting
+            System.out.println("Let's play! Type a whole number from 1 to 10000:");
+            String playerNumS = phillip.nextLine();
+            int playerNumI = Integer.parseInt(playerNumS);
 
-            //ask to play again
-            System.out.println("Would you like to play again?");
-            playGame = phillip.nextLine();
+            System.out.println("Okay, you entered " + playerNumS + "....");
+            System.out.println("");
+            List<String> guessesArray = new ArrayList<>();
+//            while (guessesArray.contains(playerNumS)) {
+//                System.out.println("And you already guessed that number. Try again");
+//                playerNumS = phillip.nextLine();
+//            }
+            guessesArray.add(playerNumS);
 
-        }
-        if (playGame.equalsIgnoreCase("no")) {
-            System.out.println("Thanks for playing " + playerName + "! Have a great day!. Good-bye!");
-            System.exit(0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException pause) {
+                pause.printStackTrace();
+            }
+            while (!(playerNumI == secretNumber)) {
+                if (playerNumI > secretNumber) {
+                    System.out.println("Try LOWER.");
+                } else {
+                    System.out.println("Try HIGHER.");
+                }
+                playerNumS = phillip.nextLine();
+                playerNumI = Integer.parseInt(playerNumS);
+            }
+//            while (playerNumI < secretNumber){
+//                    System.out.println("But it's not right. Try HIGHER.");
+//                    playerNumS = phillip.nextLine();
+//                    playerNumI = Integer.parseInt(playerNumS);
+//            }
+            while (playerNumI == secretNumber) {
+                System.out.println("And it's the right number!!  You win!");
+                System.out.println("");
+                //ask to play again
+                System.out.println("Would you like to play again?");
+                playGame = phillip.nextLine();
+
+            }
+
+            //MAIN WHILE LOOP END
+            if (playGame.equalsIgnoreCase("no")) {
+                System.out.println("Thanks for playing " + playerName + "! Have a great day!. Good-bye!");
+                System.exit(0);
+            }
         }
     }
 }
